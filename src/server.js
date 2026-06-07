297b0a4c236f11582a0fcfe8067f2575a80de841
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2026-06-07 16:08:09 +0530

    1. Updation in .env file 2. Addition of db config

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
-const express = require('express');
+require("dotenv").config();
+const connectDB = require("./config/db");
+const express = require("express");
+
 const logger = require("./middleware/logger");
+
 const driverRoutes = require("./routes/driverRoutes");
 
 const app = express();
@@ -7,14 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(logger);
 
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the F1 Drivers API!');
-});
-
 app.use("/api/drivers", driverRoutes);
 
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on ${PORT}`);
 });
