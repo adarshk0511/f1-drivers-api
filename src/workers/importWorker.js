d158a47dad88aecddc457b478683ae57a63ab231
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2026-06-19 23:29:31 +0530

    added "jobs_processed_total" query for grafana

--- src/workers/importWorker.js
+++ src/workers/importWorker.js
@@ -2,25 +2,22 @@ const { Worker } = require("bullmq");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const Job = require("../models/Job");
+const { jobsProcessedCounter } = require("../config/prometheus");
+const redisClient = require("../config/redis");
 
 mongoose.connect(process.env.MONGO_URI);
 
-const workerId =
-    process.env.WORKER_ID ||
-    "worker-unknown";
+const workerId = process.env.WORKER_ID || "worker-unknown";
 
 const worker = new Worker(
   "import-race",
 
   async (job) => {
-    const dbJob =
-    await Job.findById(
-        job.data.dbJobId
-    );
+    const dbJob = await Job.findById(job.data.dbJobId);
 
-    console.log("Bull Job ID:", job.id, workerId  );
+    console.log("Bull Job ID:", job.id, workerId);
 
-    console.log("DB Job Found:", dbJob, workerId  );
+    console.log("DB Job Found:", dbJob, workerId);
 
     if (dbJob) {
       dbJob.status = "processing";
@@ -36,6 +33,10 @@ const worker = new Worker(
       dbJob.status = "completed";
 
       await dbJob.save();
+      jobsProcessedCounter.inc();
+
+      await redisClient.incr("jobs_processed_total");
+      console.log("jobs_processed_total incremented");
     }
 
     console.log("Completed:", job.data, job.id, dbJob.status);
