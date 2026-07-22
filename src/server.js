0b75a805dd2fb676ae3ed68541e7d601dee589e2
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2026-07-23 01:27:36 +0530

    Added Gracefulshutdown for api container

--- src/server.js
+++ src/server.js
@@ -2,9 +2,7 @@ require("dotenv").config();
 const connectDB = require("./config/db");
 const redisClient = require("./config/redis");
 const logger = require("./config/logger"); // if you're using Pino
-const {
-    initializeQueues,
-} = require("./config/queueManager");
+const { initializeQueues } = require("./config/queueManager");
 const config = require("./config");
 const mongoose = require("mongoose");
 const app = require("./app");
@@ -12,28 +10,44 @@ const app = require("./app");
 const PORT = config.port || 5000;
 
 async function startServer() {
-    try {
-        // MongoDB
-        await connectDB();
-        logger.info("MongoDB Connected");
-        
-        // Redis
-        await redisClient.connect();
-        logger.info("Redis Connected");
+  try {
+    // MongoDB
+    await connectDB();
+    logger.info("MongoDB Connected");
 
-        //BullMQ Queues
-        initializeQueues();
-        logger.info("BullMQ Queues Initialized");
+    // Redis
+    await redisClient.connect();
+    logger.info("Redis Connected");
 
+    //BullMQ Queues
+    initializeQueues();
+    logger.info("BullMQ Queues Initialized");
 
-        app.listen(PORT, () => {
-            logger.info(`Server running on port ${PORT}`);
-        });
+    const server = app.listen(PORT, () => {
+      logger.info(`Server running on port ${PORT}`);
+    });
 
-    } catch (error) {
-        logger.error(error, "Failed to start server");
-        process.exit(1);
-    }
+    logger.info("6. app.listen returned");
+    
+    const shutdown = async (signal) => {
+      logger.info(`${signal} received. Starting graceful shutdown...`);
+
+      server.close(async () => {
+        logger.info("HTTP server closed.");
+
+        process.exit(0);
+      });
+    };
+
+    process.on("SIGTERM", () => shutdown("SIGTERM"));
+
+    process.on("SIGINT", () => shutdown("SIGINT"));
+
+    logger.info("Graceful shutdown handlers registered");
+  } catch (error) {
+    logger.error(error, "Failed to start server");
+    process.exit(1);
+  }
 }
 
 startServer();
